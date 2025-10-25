88a220268774eb3523ba5f84ad948795b9fe6082
ma <501847822@qq.com>
2025-10-25 23:36:50 +0800

    init project

---
+++ src/main/java/com/mahua/securitydemo/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.mahua.securitydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
